69b0eb269e7c6c10072636e3e41269a83f84c177
soribada <sd@isub-mac-mini.local>
2016-05-20 16:19:15 +0900

    - Label 추가

--- GitSample/ViewController.m
+++ GitSample/ViewController.m
@@ -17,6 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 300, 100)];
+    label.text = @"Github";
+    
+    [self.view addSubview:label];
 }
 
 - (void)didReceiveMemoryWarning {
